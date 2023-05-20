123060e107d0b0dc922294393a06f9dd66201ac4
= <diegoalt.g@hotmail.com>
2023-05-19 23:58:38 -0400

    inicio de proyecto

---
+++ src/data/productos.js
@@ -0,0 +1,43 @@
+const productos = [
+    {
+        nombre:"Poleron air jordan fly 66",
+        descripcion:" loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoeloremipcsun alej ldsoeloremipcsun alej ldsoe loremipcsun alej ldsoeloremipcsun alej ldsoeloremipcsun alej ldsoeloremipcsun alej ldsoe",
+        talla:"M",
+        precio:"49990",
+        precioAnterior:"62990",
+        id:new Date().getTime() * 2,
+        disponible:true,
+        stock:true,
+        cantidadStock:2,
+        imagenes:[],
+        categoria:""
+    },
+    {
+        nombre:"Zapatillas air force 1",
+        descripcion:" loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoeloremipcsun alej ldsoeloremipcsun alej ldsoe loremipcsun alej ldsoeloremipcsun alej ldsoeloremipcsun alej ldsoeloremipcsun alej ldsoe",
+        talla:"M",
+        precio:"55990",
+        precioAnterior:"60990",
+        id:new Date().getTime() * 3,
+        disponible:true,
+        stock:true,
+        cantidadStock:2,
+        imagenes:[],
+        categoria:""
+    },
+    {
+        nombre:"Lentes jajajala",
+        descripcion:" loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoe loremipcsun alej ldsoeloremipcsun alej ldsoeloremipcsun alej ldsoe loremipcsun alej ldsoeloremipcsun alej ldsoeloremipcsun alej ldsoeloremipcsun alej ldsoe",
+        talla:"M",
+        precio:"19990",
+        precioAnterior:"32990",
+        id:new Date().getTime() * 4,
+        disponible:true,
+        stock:true,
+        cantidadStock:2,
+        imagenes:[],
+        categoria:""
+    }
+]
+
+export default productos
